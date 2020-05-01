f4bb1abb32da4766219a9935d8bda3caec37b801
Ubuntu <amit@myUbuntu.byvwhoxkxg4uxcksr3coaclx0b.ax.internal.cloudapp.net>
2020-05-01 17:03:49 +0000

    updated few configs

--- KafkaConsumerWithStructuredStreaming/src/main/scala/ay/kafka/consumer/TwitterConsumer2.scala
+++ KafkaConsumerWithStructuredStreaming/src/main/scala/ay/kafka/consumer/TwitterConsumer2.scala
@@ -3,13 +3,8 @@ package ay.kafka.consumer
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.apache.spark.SparkConf
 import org.apache.spark.SparkContext
-/*import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.Duration*/
 import org.apache.kafka.common.serialization.StringDeserializer
 import org.apache.kafka.common.serialization.LongDeserializer
-/*import org.apache.spark.streaming.kafka010._
-import org.apache.spark.streaming.kafka010.LocationStrategies.PreferConsistent
-import org.apache.spark.streaming.kafka010.ConsumerStrategies.Subscribe*/
 import org.apache.spark.sql.functions._
 import org.apache.spark.sql.SparkSession
 
@@ -25,30 +20,17 @@ object TwitterConsumer2 {
   val df = spark
       .readStream
       .format("kafka")
-      .option("kafka.bootstrap.servers", "localhost:9092")
-      .option("subscribe", "bigdata-tweets")
+      .option("kafka.bootstrap.servers", "bootstrapServer Host:Port")
+      .option("subscribe", "topic")
       .load()
 
-  /*val query = df.writeStream
-  .outputMode("append")
-  .format("console")
-  .start()*/
   val query = df.writeStream
-    .format("parquet")
+      .format("parquet")
       .outputMode("append")
-      .option("checkpointLocation", "/home/amit/KafkaTwitterProject/checkPoints")
-      .start("/home/amit/KafkaTwitterProject/parquet/tweets")
+      .option("checkpointLocation", "path to commit checkpoints")
+      .start("path to write parquet files to")
 
   query.awaitTermination()
-
-   /* import spark.implicits._
-
-    val tweets = df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
-      .as[(String, String)]
-    tweets.show(10)*/
-
-    
-    
 
   }
 }
